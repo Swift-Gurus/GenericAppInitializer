6e8663046e99634e05a0bf9b93b00b3b759c8c3b
Alex Hmelevski <alexei.hmelevski@gmail.com>
2020-05-04 22:18:14 -0400

    add default resource type

---
+++ Sources/GenericAppInitializer/GenericBundle/DefaultResourceType.swift
@@ -0,0 +1,15 @@
+//
+//  DefaultResourceType.swift
+//  
+//
+//  Created by Alex Hmelevski on 2020-05-04.
+//
+
+import Foundation
+
+public enum DefaultResourceType: String {
+    case plist
+    case png
+    case jpeg
+    case jpg
+}
